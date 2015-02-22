2f9f6430f3344f522a886f332febc1ec11bd46f2
Boo <boo3ooo@yahoo.co.jp>
2015-02-22 13:45:41 +0900

    ディレクトリパス定数ファイル名変更

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/define.php';
+require_once __DIR__ . '/directory.php';
 require_once __DIR__ . '/bootstrap/autoload.php';
 require_once __DIR__ . '/bootstrap/page.php';
 
