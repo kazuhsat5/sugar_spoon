c7a8f1345fe1bf0cee4fa36a2b3c959a5d6023b2
Boo <boo3ooo@yahoo.co.jp>
2015-02-22 13:43:29 +0900

    コメント追加

--- define.php
+++ define.php
@@ -1,6 +1,11 @@
 <?php
 
+// コントローラーディレクトリ
 define('CONTROLLERS_DIR', __DIR__ . '/controllers/');
+
+// モデルディレクトリ
 define('MODELS_DIR',      __DIR__ . '/models/');
+
+// ビューディレクトリ
 define('VIEWS_DIR',       __DIR__ . '/views/');
 ?>
